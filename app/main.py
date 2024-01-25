131f985b1671299a65a99ced6778755608da3c22
Baldomero de castilla <bttuxs@gmail.com>
2024-01-25 07:25:56 -0600

    feat: creacion de archivos para levantar el servidor

---
+++ app/main.py
@@ -0,0 +1,38 @@
+from fastapi.openapi.utils import get_openapi
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+
+PROD = config("prod")
+
+if eval(PROD):
+    print("se inicia en modo produccion")
+    app = FastAPI(docs_url=None, redoc_url=None)
+else:
+    app = FastAPI()
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Reto xaldigital",
+        version="1.0.0",
+        description="Esta es un reto de programancion",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
