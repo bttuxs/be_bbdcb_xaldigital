bd05b942000a16d8b82cb431d003f622aa83e4f2
Baldomero de castilla <bttuxs@gmail.com>
2024-01-25 15:15:25 -0600

    fix:(MovimientosRouter) se corrige comportamiento para agregar un seed al crear la entidad

--- app/router/movimientos.py
+++ app/router/movimientos.py
@@ -1,11 +1,19 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from ..schema.MovimientosSchema import Movimientos
-from ..conf.database import get_db
+from ..models.MovimientosModel import MovimientosModel
+from ..conf.database import get_db, Base, engine
 from typing import List
 from ..repository.MovimientosRepository import getMovimientos
+from sqlalchemy import event
+from ..seeder import seed
 
+event.listen(MovimientosModel.__table__, 'after_create', seed.initialize_table)
 router = APIRouter()
+
+@router.on_event("startup")
+def configure():
+    Base.metadata.create_all(bind=engine)
 
 @router.get("", response_model=List[Movimientos])
 async def aeropuertos(bbdd: Session = Depends(get_db)):
