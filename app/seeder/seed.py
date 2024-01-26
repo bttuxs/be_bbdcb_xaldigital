c86ec28bfe59d184758dbbdc1930933e68728e34
Baldomero de castilla <bttuxs@gmail.com>
2024-01-25 19:44:31 -0600

    feat:(Seed) Se crea forma de poblar la base de datos al iniciar Vuelos

--- app/seeder/seed.py
+++ app/seeder/seed.py
@@ -26,7 +26,54 @@ INITIAL_DATA = {
             {'id_aerolineas': '2', 'nombre_aerolinea': 'Aeromar'},
             {'id_aerolineas': '3', 'nombre_aerolinea': 'Interjet'},
             {'id_aerolineas': '4', 'nombre_aerolinea': 'Aeromexico'}
-      ]
+      ],
+      'vuelos': [
+            {'id_aerolinea': '1',
+            'id_aeropuerto': '1',
+            'id_movimiento': '1',
+            'dia': '2021-05-02'
+            },
+            {'id_aerolinea': '2',
+            'id_aeropuerto': '1',
+            'id_movimiento': '1',
+            'dia': '2021-05-02'
+            },
+            {'id_aerolinea': '3',
+            'id_aeropuerto': '2',
+            'id_movimiento': '2',
+            'dia': '2021-05-02'
+            },
+            {'id_aerolinea': '4',
+            'id_aeropuerto': '3',
+            'id_movimiento': '2',
+            'dia': '2021-05-02'
+            },
+            {'id_aerolinea': '1',
+            'id_aeropuerto': '3',
+            'id_movimiento': '2',
+            'dia': '2021-05-02'
+            },
+            {'id_aerolinea': '2',
+            'id_aeropuerto': '1',
+            'id_movimiento': '1',
+            'dia': '2021-05-02'
+            },
+            {'id_aerolinea': '2',
+            'id_aeropuerto': '3',
+            'id_movimiento': '1',
+            'dia': '2021-05-04'
+            },
+            {'id_aerolinea': '3',
+            'id_aeropuerto': '4',
+            'id_movimiento': '1',
+            'dia': '2021-05-04'
+            },
+            {'id_aerolinea': '3',
+            'id_aeropuerto': '4',
+            'id_movimiento': '1',
+            'dia': '2021-05-04'
+            },
+      ],
 }
 
 def initialize_table(target, connection, **kw):
